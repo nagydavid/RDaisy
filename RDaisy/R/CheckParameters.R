d8192c2a67e91519b141e93248cfe9d412b7547a
Johannes Wilhelmus Maria Pullens <au595572@uni.au.dk>
2018-02-21 13:31:19 +0100

    CheckParameters
    Initial check to see if there are no duplicates in the file which holds the parameters to be calibrated

---
+++ RDaisy/R/CheckParameters.R
@@ -0,0 +1,22 @@
+#'@title Function to check if there are no duplicates in the parameter file
+#'
+#'@author JWM Pullens, M Jabloun
+#'
+#'@description This function checks the if there are no duplicates in the file with the new parameter values
+#'
+#'@param file The name of the file with the parameters that are used in the calibration.
+#'
+#'@examples
+#'\dontrun{
+#' CheckParameters(file)
+#'}
+#' @export
+
+
+CheckParameters<-function(file){
+  if(anyDuplicated(file$name)==0){
+    return("No duplicates were found")
+  }else{
+    stop(paste("Note: ",gsub(pattern = "$",replacement = "",x=file$name[which(duplicated(file$name)==TRUE)],fixed=T)," is duplicate",sep = ""))
+  }
+}
